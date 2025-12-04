87de62334f80431c987c05560913f5fd255f0793
Namit <chaturvedinamit899@gmail.com>
2025-12-04 14:33:40 +0530

    hi

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,39 @@
 import React, { useState } from "react";
 import Navbar from "../components/navbar";
-import { FaHistory, FaHome, FaList, FaThumbsUp } from "react-icons/fa";
+import {
+  FaHistory,
+  FaHome,
+  FaList,
+  FaThumbsUp,
+  FaUserCircle,
+} from "react-icons/fa";
 import { SiYoutubeshorts } from "react-icons/si";
 import { MdOutlineSubscriptions } from "react-icons/md";
 import { GoVideo } from "react-icons/go";
+import { IoIosAddCircle } from "react-icons/io";
 const Home = () => {
   const [sidebarOpen, setSlidebarOpen] = useState(true);
   const [selectedItem, setSelectedItem] = useState("Home");
+  const [active, setActive] = useState("Home");
+  const categories = [
+    "Music",
+    "Gaming",
+    "Movies",
+    "TV Shows",
+    "News",
+    "Trending",
+    "Entertainment",
+    "Education",
+    "Science & Tech",
+    "Travel",
+    "Fashion",
+    "Cooking",
+    "Sports",
+    "Pets",
+    "Art",
+    "Comedy",
+    "Vlogs",
+  ];
   return (
     <div className="bg-[#0f0f0f] text-white min-h-screen relative">
       <aside
@@ -75,9 +102,62 @@ const Home = () => {
           <p className="text-sm text-gray-400 px-2">Subscriptions</p>
         )}
       </aside>
+      {/* Main area */}
+      <main
+        className={`overflow-y-auto p-4 flex flex-col pb-16 transition-all duration-300 ${
+          sidebarOpen ? "md:ml-60" : "md:ml-20"
+        }`}
+      >
+        {/*Categories */}
+
+        {/* Categories */}
+        <div className="flex items-center gap-3 overflow-x-auto scrollbar-hide-black pt-2 mt-[60px]">
+          {categories.map((cat, idx) => (
+            <button
+              key={idx}
+              className="whitespace-nowrap bg-[#272727] px-4 py-1 rounded-lg text-sm hover:bg-gray-700"
+            >
+              {cat}
+            </button>
+          ))}
+        </div>
+
+        {/* CSS for Webkit */}
+      </main>
       {/* bottom nav */}
-      <nav className="md:hidden flixed bottom-0 left-0 right-0 bg-[#0f0f0f] border-t border-gray-800 justify-around py-2 z-10">
-        <div>gihi</div>
+      <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-[#0f0f0f] border-t border-gray-800 flex justify-around py-2 z-10">
+        <MobileNavItem
+          icon={<FaHome></FaHome>}
+          text={"Home"}
+          active={active == "Home"}
+          onClick={() => setActive("Home")}
+        />
+        <MobileNavItem
+          icon={<SiYoutubeshorts />}
+          text={"Shorts"}
+          active={active == "Shorts"}
+          onClick={() => setActive("Shorts")}
+        />
+        <MobileNavItem
+          icon={
+            <IoIosAddCircle
+              className="fill-gray-500 text-4xl w-9 h-9"
+              size={40}
+            />
+          }
+        />
+        <MobileNavItem
+          icon={<MdOutlineSubscriptions />}
+          text={"Subscription"}
+          active={active == "Subscriptions"}
+          onClick={() => setActive("Subscriptions")}
+        />
+        <MobileNavItem
+          icon={<FaUserCircle />}
+          text={"You"}
+          active={active == "Profile"}
+          onClick={() => setActive("Profile")}
+        />
       </nav>
       <Navbar setSlidebarOpen={setSlidebarOpen} />
     </div>
@@ -93,6 +173,19 @@ function SlidebarItem({ icon, text, open, selected, onClick }) {
     >
       <span className="text-lg">{icon}</span>
       {open && <span className="text-sm">{text}</span>}
+    </button>
+  );
+}
+function MobileNavItem({ icon, text, onClick, active }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex flex-col items-center justify-center gap-1 px-3 py-2 rounded-lg transition-all duration-300 ${
+        active ? "text-white" : "text-gray-400"
+      } hover:scale-105`}
+    >
+      <span className="text-2xl">{icon}</span>
+      {text && <span className="text-xs">{text}</span>}
     </button>
   );
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import scrollbarHide from "tailwind-scrollbar-hide";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [scrollbarHide],
+};
