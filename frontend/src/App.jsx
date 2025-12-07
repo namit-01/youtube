ed080b2d786bfcb70be7a3718c64df5e1a04f69d
Namit <chaturvedinamit899@gmail.com>
2025-12-07 16:04:08 +0530

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,42 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import { AlertProvider } from "./context/AlertContext";
-import Navbar from "./components/navbar";
 import Shorts from "./pages/shorts/Shorts";
 import GetUser from "./customHooks/GetUser";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  // Call GetUser hook to fetch user data if token exists
   GetUser();
+
+  // Get user data from Redux
+  const user = useSelector((state) => state.user.userData);
+
   return (
     <AlertProvider>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="/Shorts" element={<Shorts />} />
+        {/* Protected Home */}
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/signin" replace />}
+        >
+          <Route
+            path="/shorts"
+            element={user ? <Shorts /> : <Navigate to="/signin" replace />}
+          />
         </Route>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+
+        {/* Public Routes */}
+        <Route
+          path="/signup"
+          element={!user ? <SignUp /> : <Navigate to="/" replace />}
+        />
+        <Route
+          path="/signin"
+          element={!user ? <SignIn /> : <Navigate to="/" replace />}
+        />
       </Routes>
     </AlertProvider>
   );
