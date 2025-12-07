07d20969f9828ea3acef187cabbd4cdc9f8e99b3
Namit <chaturvedinamit899@gmail.com>
2025-12-07 15:23:55 +0530

    hi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, HashRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider } from "@react-redux";
 import { store } from "./Redux/Store.js";
 
 createRoot(document.getElementById("root")).render(
