ee4168ada3ac9bf6376bcb38c4715c0362978317
Namit <chaturvedinamit899@gmail.com>
2025-12-07 16:00:38 +0530

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,22 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import { AlertProvider } from "./context/AlertContext";
+import Navbar from "./components/navbar";
 import Shorts from "./pages/shorts/Shorts";
 import GetUser from "./customHooks/GetUser";
-import { useSelector } from "react-redux";
 
 export default function App() {
   GetUser();
-
-  const user = useSelector((state) => state.user.userData);
-
   return (
     <AlertProvider>
       <Routes>
-        {/* IF LOGGED IN → SHOW HOME, OTHERWISE → SIGNIN */}
-        <Route
-          path="/"
-          element={user ? <Home /> : <Navigate to="/signin" replace />}
-        />
-
-        {/* Shorts Protected */}
-        <Route
-          path="/shorts"
-          element={user ? <Shorts /> : <Navigate to="/signin" replace />}
-        />
-
-        {/* Public Routes */}
-        <Route
-          path="/signup"
-          element={!user ? <SignUp /> : <Navigate to="/" replace />}
-        />
-
-        <Route
-          path="/signin"
-          element={!user ? <SignIn /> : <Navigate to="/" replace />}
-        />
+        <Route path="/" element={<Home />}>
+          <Route path="/Shorts" element={<Shorts />} />
+        </Route>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </AlertProvider>
   );
