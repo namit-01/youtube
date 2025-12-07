0b7df2759f5a59faf020c1c0ca5d05806de38d2e
Namit <chaturvedinamit899@gmail.com>
2025-12-07 16:09:22 +0530

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,16 @@ import SignIn from "./pages/SignIn";
 import { AlertProvider } from "./context/AlertContext";
 import Shorts from "./pages/shorts/Shorts";
 import GetUser from "./customHooks/GetUser";
-import { useSelector } from "react-redux";
 
 export default function App() {
   // Call GetUser hook to fetch user data if token exists
   GetUser();
 
-  // Get user data from Redux
-  const user = useSelector((state) => state.user.userData);
+  // Check if token exists in cookies
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("token="))
+    ?.split("=")[1];
 
   return (
     <AlertProvider>
@@ -20,22 +22,22 @@ export default function App() {
         {/* Protected Home */}
         <Route
           path="/"
-          element={user ? <Home /> : <Navigate to="/signin" replace />}
+          element={token ? <Home /> : <Navigate to="/signin" replace />}
         >
           <Route
             path="/shorts"
-            element={user ? <Shorts /> : <Navigate to="/signin" replace />}
+            element={token ? <Shorts /> : <Navigate to="/signin" replace />}
           />
         </Route>
 
         {/* Public Routes */}
         <Route
           path="/signup"
-          element={!user ? <SignUp /> : <Navigate to="/" replace />}
+          element={!token ? <SignUp /> : <Navigate to="/" replace />}
         />
         <Route
           path="/signin"
-          element={!user ? <SignIn /> : <Navigate to="/" replace />}
+          element={!token ? <SignIn /> : <Navigate to="/" replace />}
         />
       </Routes>
     </AlertProvider>
