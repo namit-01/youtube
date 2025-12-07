952c8e14b6e08bce3c79f31b59a40918709b2b19
Namit <chaturvedinamit899@gmail.com>
2025-12-07 15:28:40 +0530

    hi

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,4 +1,4 @@
-import configureStore from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import UserSlice from "./UserSlice";
 export const store = configureStore({
   reducer: {
