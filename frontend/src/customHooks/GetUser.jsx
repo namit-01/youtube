40bb0565fbc7c7d6b539f7e925e957efc1abde97
Namit <chaturvedinamit899@gmail.com>
2025-12-29 23:50:26 +0530

    djvfj

--- frontend/src/customHooks/GetUser.jsx
+++ frontend/src/customHooks/GetUser.jsx
@@ -1,37 +1,30 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setUserData } from "../Redux/UserSlice";
 
 const GetUser = () => {
   const dispatch = useDispatch();
+
   const API_URL = import.meta.env.VITE_API_URL;
-  const channelData = useSelector((state) => state.channel);
+
   useEffect(() => {
+    // Check if token exists in cookies
+
     const fetchUser = async () => {
       try {
-        const token = localStorage.getItem("token"); // get token from localStorage
-        if (!token) {
-          dispatch(setUserData(null));
-          return;
-        }
-
         const result = await axios.get(`${API_URL}/api/user/getUser`, {
-          headers: {
-            Authorization: `Bearer ${token}`, // send token in headers
-          },
+          withCredentials: true,
         });
-
         dispatch(setUserData(result.data.user));
         console.log("User data fetched:", result.data.user);
       } catch (err) {
         console.log("Error fetching user:", err);
-        dispatch(setUserData(null));
       }
     };
 
     fetchUser();
-  }, [channelData]);
+  }, []);
 };
 
 export default GetUser;
